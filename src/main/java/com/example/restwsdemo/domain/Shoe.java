c07f8c03d091ee74b76c135a1ca12ba930ef1ce6
AdZelek <ada.zelek@gmail.com>
2017-11-14 16:56:03 +0100

    add EntityManager, add hsqldb

--- src/main/java/com/example/restwsdemo/domain/Shoe.java
+++ src/main/java/com/example/restwsdemo/domain/Shoe.java
@@ -1,13 +1,19 @@
 package com.example.restwsdemo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.persistence.Id;
 
 @XmlRootElement
+@Entity
 	public class Shoe {
+		
+		public Long Id; 
 		public String name;
 		public int size;
 		public double price;
-		
 		
 		public Shoe() {
 			
@@ -16,6 +22,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 			this.name = name;
 			this.size = size;
 			this.price = price;
+		}
+		@Id
+		@GeneratedValue(strategy =GenerationType.IDENTITY) 
+		public Long getId() {
+			return Id;
+		}
+		public void setId(Long id) {
+			Id = id;
 		}
 		public double getPrice() {
 			return price;
--- src/main/java/com/example/restwsdemo/service/ShoeManager.java
+++ src/main/java/com/example/restwsdemo/service/ShoeManager.java
@@ -5,16 +5,22 @@ import java.util.Collections;
 import java.util.List;
 
 import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import com.example.restwsdemo.domain.Shoe;
 
 @Singleton
 public class ShoeManager {
 	
+	
 	private List<Shoe> db = (new ArrayList<>());
+	
+	@PersistenceContext
+	EntityManager em;
 
 	public void addShoe(Shoe shoe) {
-		db.add(shoe);
+		em.persist(shoe);
 	}
 
 	public void deleteShoe(Shoe shoe){
